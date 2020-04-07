cb691f6b4cd1751d0846d506c224ee47e27d4a94
nasirhm <nasirhussainm14@gmail.com>
2020-04-07 05:11:07 +0500

    Exercise 1

---
+++ exercise-1/main.py
@@ -0,0 +1,14 @@
+# Hello World
+
+print "Hello World"
+print "Learning Python 2"
+print "Starting with basics of Python"
+print "Looking forward to be a Pythonista"
+
+# Expected output :
+# If run : python2 main.py
+
+# Hello World
+# Learning Python 2
+# Starting with basics of Python
+# Looking forward to be a Pythonista
